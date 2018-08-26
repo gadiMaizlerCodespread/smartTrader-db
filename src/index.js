24f24514cc7490db23f0758fc660a55b82187948
Izik Orgad <izik.orgad@bitmain.com>
2018-08-26 12:55:10 +0300

    fix CRLF lint error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 // Set process name
-process.title  = ['My Process']; // TO-DO: change to your process real name.  
+process.title = ['My Process']; // TO-DO: change to your process real name.
 
 import server from 'server';
 server.start();
