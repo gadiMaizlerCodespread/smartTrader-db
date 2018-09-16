d6b295b854e82b9bc08c5766548912bd953d0a86
Izik Orgad <izik.orgad@bitmain.com>
2018-09-16 16:38:24 +0300

    Reverting to the state of the project at adfa6d1a983775c011412b53e83f0f242d993cb5

---
+++ src/ConfigurationManager/configuration-manager.js
@@ -0,0 +1,10 @@
+class configurationManager{
+    constructor() {
+    }
+}
+
+configurationManager.prototype.createUsersTable = function() {
+};
+
+
+module.exports = configurationManager;
---
+++ src/UserManager/user-manager.js
@@ -0,0 +1,9 @@
+class userManager{
+    constructor() {
+    }
+}
+
+userManager.prototype.createUsersTable = function() {
+};
+
+module.exports = userManager;
